6662a05089a54bed3ebb109a69d4d065cae53b5d
Lev Eidelman Nagar <Lev.Eidelman.Nagar@sapiens.com>
2019-02-12 10:59:29 +0200

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,8 @@
 module.exports = {
-  env: {
-    browser: true,
-  },
-  extends: [
-    'airbnb',
-    'plugin:jsx-a11y/recommended',
-    'plugin:jest/recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-  ],
-  plugins: ['jsx-a11y', 'jest'],
+  parser: 'babel-eslint',
+  extends: ['airbnb-base', 'plugin:jest/recommended', 'plugin:prettier/recommended'],
+  plugins: ['jest'],
   rules: {
-    'react/jsx-filename-extension': 'off',
-    'react/jsx-one-expression-per-line': 'off',
-    'import/no-extraneous-dependencies': 'off',
     'prettier/prettier': 'error',
   },
 };
