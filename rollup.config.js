79d47643eb80e3bb84d6d2ce915a8fe8f5fbe92e
Lev Eidelman Nagar <Lev.Eidelman.Nagar@sapiens.com>
2019-02-13 15:29:26 +0200

    removed babel lodash plugin

--- rollup.config.js
+++ rollup.config.js
@@ -5,27 +5,11 @@ import cleanup from 'rollup-plugin-cleanup';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import pkg from './package.json';
 
-const babelConfig = {
-  exclude: ['node_modules/**'],
-  plugins: ['lodash'],
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-        modules: false,
-      },
-    ],
-  ],
-};
-
 export default [
   {
     input: 'src/main.js',
     treeshake: true,
     output: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }],
-    plugins: [peerDepsExternal(), resolve(), commonjs(), babel(babelConfig), cleanup()],
+    plugins: [peerDepsExternal(), resolve(), commonjs(), babel(), cleanup()],
   },
 ];
