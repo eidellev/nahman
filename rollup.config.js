756df75a5ca36c46ef1b3f0522b3e056e540448a
Lev Eidelman Nagar <Lev.Eidelman.Nagar@sapiens.com>
2019-02-13 16:09:07 +0200

    some more rollup config fiddling to handle external libs better

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,14 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import cleanup from 'rollup-plugin-cleanup';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import pkg from './package.json';
 
 export default [
   {
     input: 'src/main.js',
+    external: ['lodash', 'lodash/fp'],
     treeshake: true,
     output: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }],
-    plugins: [peerDepsExternal(), resolve(), commonjs(), babel(), cleanup()],
+    plugins: [resolve(), commonjs(), babel(), cleanup()],
   },
 ];
